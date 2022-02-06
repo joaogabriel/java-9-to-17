fb88a6b5e08f582b1a6e083a6b3470ad1ebd885a
João Gabriel Esteves <jgs.esteves@gmail.com>
2022-02-06 17:01:22 -0300

    Add helpful npe feature

---
+++ src/main/java/com/java/newfeatures/helpfulnpe/HelpfulNullPointerException.java
@@ -0,0 +1,20 @@
+package com.java.newfeatures.helpfulnpe;
+
+import com.java.newfeatures.helpfulnpe.model.Address;
+import com.java.newfeatures.helpfulnpe.model.Developer;
+import com.java.newfeatures.helpfulnpe.model.Manager;
+import com.java.newfeatures.helpfulnpe.model.Squad;
+
+public class HelpfulNullPointerException {
+
+    public static void main(String[] args) {
+        Developer developer = new Developer();
+        developer.setSquad(new Squad());
+        developer.getSquad().setManager(new Manager());
+
+        Address address = developer.getSquad().getManager().getAddress();
+
+        System.out.println(address.getLabel());
+    }
+
+}
---
+++ src/main/java/com/java/newfeatures/helpfulnpe/model/Manager.java
@@ -0,0 +1,33 @@
+package com.java.newfeatures.helpfulnpe.model;
+
+public class Manager {
+
+    private String name;
+    private String login;
+    private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
